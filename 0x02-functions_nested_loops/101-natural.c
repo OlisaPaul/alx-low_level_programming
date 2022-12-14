198ce5ed82a574a7dacb41a9f5fdc64abd09a868
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-14 08:18:17 -0800

    added new file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,23 +2,24 @@
 
 /**
  * main - print sum of multiples of 3 or 5 below 1024
- * @n: integer argument
+ * Return: 0
  */
 
 int main(void)
 {
-    int sum = 0;
-    int i;
+	int n;
+	int sum;
 
-    for (i = 0; i <= 1024; i++)
-    {
-        if(i % 5 == 0 || i % 3 == 0 )
-        {
-            sum = sum + i;
-        }
-    }
-    printf("%d\n", sum);
+	sum = 0;
 
-    return (0);
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
 
+	return (0);
 }

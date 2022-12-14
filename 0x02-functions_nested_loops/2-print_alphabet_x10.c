4df8458803178423c9851ecd51358097dc637827
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-13 20:15:53 -0800

    added new file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+        int i;
+	char alpha;
+
+	i = 0;
+
+	while(i < 10){
+
+        	alpha = 'a';
+	
+       	 while(alpha <= 'z'){
+       		 _putchar(alpha);
+               	 alpha++;
+       	 }
+       	 _putchar('\n')
+	}
+}
+

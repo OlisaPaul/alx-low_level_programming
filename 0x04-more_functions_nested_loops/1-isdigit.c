714c692d15345e17f74f63b38609467f15a2b447
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-15 19:57:55 -0800

    added new file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - define if a character is uppercase
+ * _isupper - define if a character is a number
  *
  * Description: Prints the alphabet with _putchar
  * @c: charater
  * Return: void
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);

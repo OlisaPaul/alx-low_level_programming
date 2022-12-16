7c5ccdfe20bd6d480985ef682c3bead289201121
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-15 20:21:11 -0800

    added new file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_numbers - input
  * Description: prints number from 0 to 9
- * Return 0
+ * Return void
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; 1++)
+	for (i = '0'; i <= '9'; 1++)
 	{
 		_putchar(i);
 	}

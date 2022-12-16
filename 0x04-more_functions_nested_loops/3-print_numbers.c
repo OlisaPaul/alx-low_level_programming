8550fda32ff1555afd22b2df9c37b4e848bcbca2
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-15 20:19:00 -0800

    added new file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - input
+ * Description: prints number from 0 to 9
+ * Return 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; 1++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}

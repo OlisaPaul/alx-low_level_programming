5af3bb04e2fb102045470b80800960e9465e894b
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-19 19:06:43 -0800

    added new file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,15 +5,22 @@
  * _puts - prints the string passed into it
  * @str: parameter of string type
  * Return: always 0
- */
-void print_rev(char *str)
+*/
+void print_rev(char *s)
 {
 	int i;
-	int len = strlen(str);
 
-	for (i = len; i < '0'; i--)
+	i = 0;
+
+	while (*(s + i) != '\0')
 	{
-	    _putchar(*(str + i));
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
 	}
 	_putchar('\n');
 }

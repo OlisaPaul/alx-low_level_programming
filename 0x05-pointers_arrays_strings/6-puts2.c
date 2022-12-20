8da6e98487efe2cdab0ca986c968151d097f35f7
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-19 19:16:16 -0800

    added new file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the string passed into it
+ * @str: parameter of string type
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+        int i;
+
+        for (i = 0; *(str + i) != '\0'; i = i + 2)
+        {
+                _putchar(*(str + i));
+        }
+        _putchar('\n');
+}
+

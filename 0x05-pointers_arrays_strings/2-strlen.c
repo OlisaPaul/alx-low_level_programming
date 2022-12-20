8bdd78f910af062efd87c3246c54fad77d25d544
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-19 17:39:33 -0800

    added new file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,10 @@
 
 /**
  * _strlen - returns the length of a string
- *
+ *@s: string parameter
  * Return: int
  */
-int _strlen(char *s){
-	return strlen(s);
+int _strlen(char *s)
+{
+	return (strlen(s));
 }

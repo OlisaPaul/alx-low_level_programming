1a323f7655e0553e980aba88c648c7bf2a1ce67b
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-19 17:50:57 -0800

    added new file

---
+++ 0x05-pointers_arrays_strings/3-put.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * Return: int
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for(i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	{
+	_putchar('\n');
+}

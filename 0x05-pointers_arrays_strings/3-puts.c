09b9432714fe0c47bc598c688605c07a86126957
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-19 18:15:10 -0800

    added new file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
-#include <string.h>
+#include "main.h"
 
 /**
- * _strlen - returns the length of a string
- *
- * Return: int
+ * _puts - prints the string passed into it
+ * @str: parameter of string type
+ * Return: always 0
  */
 void _puts(char *str)
 {
 	int i;
 
-	for(i = 0; i < strlen(str); i++)
+	for(i = 0; *(str + i) != '\0'; i++)
 	{
-		_putchar(str[i]);
+		_putchar(*(str + i));
 	{
 	_putchar('\n');
 }

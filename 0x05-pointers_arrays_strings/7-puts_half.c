ce5f506f50272feb3592ca3864f0ad3eb5228b3e
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-19 20:20:13 -0800

    added new file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints the string passed into it
+ * @str: parameter of string type
+ * Return: always 0
+ */
+void puts_half(char *str)
+{
+    int i;
+    int len;
+    int n;
+    for (len = 0; *(str + len) != 0; len++)
+    {
+        len++;
+    }
+
+    if (len % 2 == 0)
+    {
+        n = len / 2;
+    }
+    else
+    {
+        n = (len + 1) / 2;
+    }
+
+    for (i = n; i < len; i--)
+    {
+        _putchar(*(str + i));
+    }
+    _putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char s[] = "hello paul";
+	int len = strlen(s);
+
+	putchar(len / 2);
+
+	return (0);
+
+}

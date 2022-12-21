5b133eccc96551fdc3fbed5265abb2e05f17beb3
OlisaPaul <odirahchukwumma28@gmail.com>
2022-12-20 20:37:40 -0800

    added new file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - converts string to an integer
+ * @s: the string parameter
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+{
+	return atoi(s);
+}

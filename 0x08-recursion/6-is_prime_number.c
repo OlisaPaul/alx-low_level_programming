5c9659b2a957191ef348d5e6747ca9d2875a5c27
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-04 18:02:54 -0800

    added new file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,28 +12,34 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return (prime(n, 2));
-}
-/**
- * prime - calculates the sqrt of a number.
- * @n: natural number
- * @i: counter or number to be multiplied.
- *
- * Return: sqrt 0 if sqrt is not natural.
- */
-int prime(int x, int y)
-{
-
-	if(x % y == 0)
-	{
-		return (0);
-	}
-	else if(y > (x / 2))
+	else if (n == 2)
 	{
 		return (1);
 	}
 	else
 	{
-		return(prime(x, y + 1));
+		return (is_prime_manual(n, 2));
+	}
+}
+/**
+ * is_prime_manual - calculate if the number is prime.
+ * @n: natural number
+ * @i: counter or divisor of n.
+ *
+ * Return: 1 if is prime 0 otherwise.
+ */
+int is_prime_manual(int n, int i)
+{
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (i == (n - 1) && n % i != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_manual(n, i + 1));
 	}
 }

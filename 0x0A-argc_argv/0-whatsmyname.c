bf486284e46153a73242a3aa25121718dd3c43a4
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-10 04:57:12 -0800

    added new file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,10 @@
  * Return: 0 on success
  */
 
-int  main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
 
-	return (0);	
+	return (0);
 }

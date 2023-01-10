8346001235726e8d221e983ed2598f1dbb44f6b2
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-10 01:06:08 -0800

    added new file

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - check digit
+ * @c: character
+ * Return: int that tells num spaces in between
+ */
+int _isdigit(int c)
+{
+	return (c);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *@s: string parameter
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

cd30419b12c9dd4177ab6ef4b731d02767f3ed5e
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-10 05:15:43 -0800

    added new file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,8 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-        if (argc > 0)
-                printf("%d\n", argc - 1);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 
-        return (0);
+	return (0);
 }
-

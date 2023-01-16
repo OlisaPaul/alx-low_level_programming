40db1d31245248e5ae31e142e2f991ca5f8849c2
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-16 08:42:30 -0800

    added new file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,42 @@
  *
  * return: structure
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	dog_t *newDog;
+	int lName, lOwner, i;
 
-	return (dog);
-}
+	newDog = malloc(sizeof(struct dog));
+	if (newDog == NULL)
+		return (NULL);
+
+	for (lName = 0; *(name + lName); lName++)
+		;
+	lName++;
+	for (lOwner = 0; *(owner + lOwner); lOwner++)
+		;
+	lOwner++;
+
+	newDog->name = malloc(lName * sizeof(char));
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	for (i = 0; i < lName; i++)
+		*(newDog->name + i) = *(name + i);
+
+	newDog->age = age;
+
+	newDog->owner = malloc(lOwner * sizeof(char));
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+	for (i = 0; i < lOwner; i++)
+	*(newDog->owner + i) = *(owner + i);
+
+	return (newDog);
+

fbba2e6a6fe5835f5da61c709dfe7bccac107609
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-16 08:53:37 -0800

    added new file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - it frees memory in the heap
+ * @d: a structure
+ *
+ *Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+
+	free(d->name);
+	free(d->age);
+	free(d);
+
+}

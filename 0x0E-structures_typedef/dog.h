52ba243dc8f595b4aa038a924a0c832913569617
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-16 06:46:04 -0800

    added new file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Structure for my dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};

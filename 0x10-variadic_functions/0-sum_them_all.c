0c9ec8eb10f463a2b34409cd546130a9758fcf6d
OlisaPaul <odirahchukwumma28@gmail.com>
2023-01-19 13:21:48 -0800

    added new file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,6 @@
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: number of variables
- *
  * Return: an integer
  */
 
@@ -12,9 +11,6 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum = 0;
 	va_list ap;
-
-	if (n == 0)
-		return (0);
 
 	va_start(ap, n);
 
